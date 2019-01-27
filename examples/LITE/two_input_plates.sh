0fa6fc04c139ec3adf95cc75cc736beaf33fe845
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2019-01-27 07:50:55 +0100

    Added 5ml and 10ml tube racks to db

--- examples/LITE/two_input_plates.sh
+++ examples/LITE/two_input_plates.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # 96-well source plate
 pyTecanFluent LITE --prefix /tmp/LITE_1plate \
+  --pcr-mm-labware-type Falcon_10ml_3X_tube_rack \
   $DIR/../../tests/data/LITE_2-input-plates.xlsx
 
 
