b0e16efa8c46c74955789ee8d7cc885a7b9bb20d
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2021-11-18 20:47:13 +0100

    supporting py3.6 and py3.7

--- setup.py
+++ setup.py
@@ -43,11 +43,9 @@ setup(
     zip_safe=False,
     keywords='pyTecanFluent',
     classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     test_suite='tests',
     tests_require=test_requirements
